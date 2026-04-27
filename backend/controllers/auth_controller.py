34e55ed3527f06bf39d498e71df982032e6740fc
Gourav83bidhoniya <gourav.p9483@gmail.com>
2026-04-27 18:31:57 +0530

    Remove user registration and login functions
    Removed the register_user and login_user functions from auth_controller.py.

--- backend/controllers/auth_controller.py
+++ backend/controllers/auth_controller.py
@@ -1,34 +1,4 @@
 from flask import request, jsonify
-
-import random
-import string
-
-def register_user():
-    data = request.json
-    
-    # Check if phone exists
-    if users_col.find_one({"phone": data.get("phone")}):
-        return jsonify({"error": "Phone number already registered!"}), 400
-
-    # Generate User ID
-    user_id = 'usr_' + ''.join(random.choices(string.ascii_lowercase + string.digits, k=9))
-    new_user = {**data, "userId": user_id}
-    
-    users_col.insert_one(new_user)
-    new_user['_id'] = str(new_user['_id'])
-    
-    return jsonify({"message": "Account created!", "user": new_user}), 201
-
-def login_user():
-    data = request.json
-    user = users_col.find_one({"phone": data.get("phone"), "password": data.get("password")})
-    
-    if not user:
-        return jsonify({"error": "Invalid phone number or password"}), 400
-        
-    user['_id'] = str(user['_id'])
-    return jsonify({"message": "Login successful", "user": user}), 200
-from flask import request, jsonify
 from pymongo import MongoClient
 import os
 import random
